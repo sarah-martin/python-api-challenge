568f6143f1beca4d31a06eea563d5fda293084b4
sarah-martin <smartin@globaltieskc.org>
2020-06-09 23:33:12 -0500

    Final update

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "a80addeb5eb0a438374d9369aae14a4c"
+weather_api_key = "KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyD6Ewm-4RhZ026ev_jryxRemU_xf88X0SA"
+g_key = "KEY HERE"
